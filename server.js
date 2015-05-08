f89c3cea306af29f5fab637c42f7ad3d65c4698d
nchronas <nchronas@gmail.com>
2015-05-08 12:35:53 +0300

    creating app

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+
+var app = express();
+
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+console.log("Hello Resin");
+
